12c1a9ee58698ab6d77edcd3757a090c35d195c2
lukeg007 <luke@gilsinger.net>
2022-01-30 16:04:32 -0500

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mount -o rw,remount /
 mount -o rw,remount /system
 cp android.py python/lib
 cp -r python/ /system/python
+ln -s /system/python/bin/python3 /system/bin/python3.9
 ln -s /system/python/bin/python3 /system/bin/python3
-chmod 755 /system/bin/python3.9
 chmod 755 /system/python/bin/*

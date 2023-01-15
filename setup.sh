5d17cb6aedaa3dd12d5317b2eab72ba71dd24d74
LukeG007 <69555173+LukeG007@users.noreply.github.com>
2023-01-14 22:33:49 -0500

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 mount -o rw,remount /
 mount -o rw,remount /system
 cp android.py python/system/python3.4.2/lib/python3.4
-cp -r python/ /
+cp -r python/system /
 chmod 755 /system/bin/python3
 chmod 755 /system/python3.4.2/bin/*
